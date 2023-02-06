65313fa7fb5d4b0af28ba5a40607d322bfd0b55b
Kevin <falcony45@gmail.com>
2023-02-06 13:42:09 -0600

    agregamos info de proyectos arquitectonicos

---
+++ proyectos-arquitectonicos.php
@@ -0,0 +1,56 @@
+<?php require('./layout/header.php') ?>
+<main>
+        <section class="productos">
+            <div class="container-title mb-5">
+                <h1 class="productos-title productos-arquitectonicos">Productos productos arquitectonicos</h1>
+            </div>
+            <div class="container">
+
+                <div class="row g-3 mt-3 d-flex productos-container">
+
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="main-title">PLANOS DE PLOMERIA</h2>
+                            </div>
+                        </div> 
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="main-title">ELECTRICIDAD</h2>
+                            </div>
+                        </div> 
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="main-title">CONSTRUCCIÓN EN GENERAL</h2>
+                            </div>
+                        </div> 
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="main-title">PRESUPUESTRO DE OBRAS</h2>
+                            </div>
+                        </div> 
+                    </div>
+
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="main-title">DESTAJOS</h2>
+                            </div>
+                        </div> 
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    </main>
+<?php require('layout/footer.php') ?>
